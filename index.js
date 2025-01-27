03a8284a1af4bf2e987b585b23c4bc51ffa2af78
Muhammad Islam Kamran <islamkamran332@gmail.com>
2025-01-27 15:21:41 +0500

    functions in JS

--- index.js
+++ index.js
@@ -1,51 +1,68 @@
-// comment ignored by js engine v8
-console.log('Islam kamran');
-var x =6
-console.log(x)
+// // comment ignored by js engine v8
+// console.log('Islam kamran');
+// var x =6
+// console.log(x)
 
-let names='islam';
+// let names='islam';
 
-console.log(names);
+// console.log(names);
 
-// cannot be a revered key word cannot use
-const interest = 0.3;
-console.log(interest)
-
-
-let data = 'islam';
-let age = 30;
-let isApproved = true;
-let fName = undefined;
-let lName = null
-
-console.log(typeof(data))
-// console.log(age)
-// console.log(isApproved)
-// console.log(fName)
-// console.log(lName)
+// // cannot be a revered key word cannot use
+// const interest = 0.3;
+// console.log(interest)
 
 
-data=12;
-console.log(typeof(data))
+// let data = 'islam';
+// let age = 30;
+// let isApproved = true;
+// let fName = undefined;
+// let lName = null
+
+// console.log(typeof(data))
+// // console.log(age)
+// // console.log(isApproved)
+// // console.log(fName)
+// // console.log(lName)
 
 
-let firstName = "islam"
+// data=12;
+// console.log(typeof(data))
 
-let person = {
-    names: "islam",
-    age: 30
-}; //object literal same as dict in python
 
-// dot notataion access default choice 
-person.names  = "kamran"
+// let firstName = "islam"
 
-// braket notation
-person['names'] = 'Muhammad'
+// let person = {
+//     names: "islam",
+//     age: 30
+// }; //object literal same as dict in python
 
-console.log(person)
+// // dot notataion access default choice 
+// person.names  = "kamran"
+
+// // braket notation
+// person['names'] = 'Muhammad'
+
+// console.log(person)
 
 
 // arrays
 
-let selectedColors = ['red','blue'];
-console.log(selectedColors)
+// let selectedColors = ['red','blue'];
+
+// selectedColors[2] = 'green';
+// selectedColors[3] = 2;
+// console.log(selectedColors);
+
+
+function greet(x='islam'){
+    console.log("hello " + x);
+}
+
+function mult(number){
+    return number*number;
+}
+
+let names = "kamran"
+greet(names);
+
+console.log(mult(12);)
