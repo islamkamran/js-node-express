ba3d365bac4cffb2b55558f2e2fa07d341e32f43
Muhammad Islam Kamran <islamkamran332@gmail.com>
2025-01-27 15:08:31 +0500

    js practice

--- hello.js
+++ hello.js
@@ -1,1 +1,2 @@
-console.log("This is Muhammad Islam Kamran starting to learn backend development with JS-NODE and EXPRESS")
+console.log("This is Muhammad Islam Kamran starting to learn \nbackend development with JS-NODE and EXPRESS")
+
---
+++ index.js
@@ -0,0 +1,51 @@
+// comment ignored by js engine v8
+console.log('Islam kamran');
+var x =6
+console.log(x)
+
+let names='islam';
+
+console.log(names);
+
+// cannot be a revered key word cannot use
+const interest = 0.3;
+console.log(interest)
+
+
+let data = 'islam';
+let age = 30;
+let isApproved = true;
+let fName = undefined;
+let lName = null
+
+console.log(typeof(data))
+// console.log(age)
+// console.log(isApproved)
+// console.log(fName)
+// console.log(lName)
+
+
+data=12;
+console.log(typeof(data))
+
+
+let firstName = "islam"
+
+let person = {
+    names: "islam",
+    age: 30
+}; //object literal same as dict in python
+
+// dot notataion access default choice 
+person.names  = "kamran"
+
+// braket notation
+person['names'] = 'Muhammad'
+
+console.log(person)
+
+
+// arrays
+
+let selectedColors = ['red','blue'];
+console.log(selectedColors)
