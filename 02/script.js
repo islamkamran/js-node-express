d52d759d0fd99f80b3cd4a9e88323de01b6ed2e5
Muhammad Islam Kamran <islamkamran332@gmail.com>
2025-01-28 12:57:32 +0500

    day two asks

---
+++ 02/script.js
@@ -0,0 +1,18 @@
+// variables
+// var names = "islam";
+// var age = 30;
+// console.log(names)
+// console.log(age)
+
+// console.log(age);
+
+// var age = 30;
+
+// console.log(age);
+
+// if (true) {
+//   let a = 45;
+//   console.log(a);
+// }
+
+const age = 30;
