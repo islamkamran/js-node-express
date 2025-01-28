2b6509cfe3164afbcb0bfab3c1a0d6239362a683
Muhammad Islam Kamran <islamkamran332@gmail.com>
2025-01-28 23:03:57 +0500

    logical operators

---
+++ 05/script.js
@@ -0,0 +1,14 @@
+const age = 2;
+const gender = "female";
+
+if (age >= 18 || gender == "male") {
+  console.log("You are an adult male");
+} else if (age >= 18 || gender == "female") {
+  console.log("You are a female adult");
+} else {
+  console.log("Chal mera putt shotti kaaar");
+}
+
+const num = 4;
+if (num % 2 != 0) console.log("Odd");
+else console.log("Even");
