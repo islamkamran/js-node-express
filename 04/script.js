7005a1090db9b266826beac725e0b1a518759c0b
Muhammad Islam Kamran <islamkamran332@gmail.com>
2025-01-28 22:18:12 +0500

    lecture 4 conditional statements and switchcases

---
+++ 04/script.js
@@ -0,0 +1,29 @@
+// const age = 79;
+
+// if (age >= 18) {
+//   console.log("Yes you are eligible to vote");
+// } else if (age >= 80) {
+//   console.log("Pora hero ye");
+// } else {
+//   console.log("No you are not eligible to vote");
+// }
+
+// // ternary operators
+// let result = age>=80 ? "greater": "lesser"
+
+// console.log(result)
+
+// switch case in JS
+const number = 22;
+
+switch (number) {
+  case 1:{
+    console.log("english");}
+    break;
+  case 2:
+    console.log("urdu");
+    break;
+  default:
+    console.log("default case");
+    break;
+}
